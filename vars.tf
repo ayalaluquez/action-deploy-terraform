fc17aa7534ad61d2682e9989fcfa6ca50896f115
ayalaluquez <blanca.ayala@kronoscode.com>
2020-05-22 14:37:28 -0300

    initial terraform configuration

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-05d7ab19b28efa213"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
+
