05c07878f40176498e0f337bb485bf8759fce846
Blanca Ayala <45976038+ayalaluquez@users.noreply.github.com>
2020-05-24 14:25:23 -0300

    update variables

--- vars.tf
+++ vars.tf
@@ -1,8 +1,9 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 variable "AWS_REGION" {
   default = "us-east-1"
 }
+
 
 variable "AMIS" {
   type = "map"
